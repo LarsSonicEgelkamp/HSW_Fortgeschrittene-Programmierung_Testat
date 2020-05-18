e7a0e2f14145aff8f4875d31114bd5b60c8e955f
Lars Egelkamp <egelkamp00@gmx.de>
2020-05-18 18:19:44 +0200

    Das ist jetzt die Version wo man ein neuen Branch erstellt
    Die anderen habe ich in den Master gemergt und gelöscht

--- src/First_Class/Starter_Class.java
+++ src/First_Class/Starter_Class.java
@@ -3,13 +3,7 @@ package First_Class;
 public class Starter_Class {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		
-		
-=======
-
-		System.out.println("Lars");
->>>>>>> LarsBranch
 	}
 
 }
--- src/First_Class/class1.java
+++ src/First_Class/class1.java
@@ -3,8 +3,7 @@ package First_Class;
 public class class1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		
 	}
 
 }
