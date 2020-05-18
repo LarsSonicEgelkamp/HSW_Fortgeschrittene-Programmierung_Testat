be39b1bf8da80f6230785d72cd72f3e8414f98cc
Lars Egelkamp <egelkamp00@gmx.de>
2020-05-18 18:39:28 +0200

    Schon mal ein Frame erstellt
    Ich bau dann noch ein Action Listener ein also kann man einfach Methoden schreiben um die Funktionen auszuführen

--- src/user_Interface/GUI.java
+++ src/user_Interface/GUI.java
@@ -1,8 +1,20 @@
 package user_Interface;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class GUI {
 
 	public static void main(String[] args) {
+		
+		// Erzeugung eines neuen Frames mit dem Titel "Beispiel JFrame "        
+		JFrame meinFrame = new JFrame("Börsenanwendung");
+		// größe setzen        
+		meinFrame.setSize(200,200);
+		// Wir lassen unseren Frame anzeigen
+		meinFrame.setVisible(true);
+		//Passwort eingabe
+		JOptionPane.showInputDialog("Bitte User-ID angeben");
 		
 	}
 
