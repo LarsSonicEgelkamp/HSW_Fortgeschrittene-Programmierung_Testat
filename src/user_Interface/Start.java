3e6867b914b84e6bc11290f0f8f3889cb52d52c0
Lars Egelkamp <egelkamp00@gmx.de>
2020-05-25 23:27:37 +0200

    Serialisierungs Klasse hinzugefügt

--- src/user_Interface/Start.java
+++ src/user_Interface/Start.java
@@ -5,7 +5,9 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		GUI start = new GUI();
+		GUI start = new GUI();
+        start.setVisible(true);
+
 
 
 	}
