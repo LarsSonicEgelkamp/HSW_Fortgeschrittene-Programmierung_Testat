4a41a76e4a239166d270fa2a99045223197c28b2
Gina <ginac.bauer@t-online.de>
2020-05-26 13:26:51 +0200

    Test Package hinzugefügt; Datenbankverbindung hinzugefügt

--- src/user_Interface/Start.java
+++ src/user_Interface/Start.java
@@ -1,9 +1,23 @@
 package user_Interface;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String databaseURL = "jdbc:mysql://localhost/boersendatenbank?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		try (Connection con = DriverManager.getConnection(databaseURL, "user", "user")) {
+
+			Statement stat = con.createStatement();
+
+		} catch (SQLException e) {
+			System.out.println("Fehler: " + e.getMessage());
+		}
 		
 		GUI start = new GUI();
         start.setVisible(true);
