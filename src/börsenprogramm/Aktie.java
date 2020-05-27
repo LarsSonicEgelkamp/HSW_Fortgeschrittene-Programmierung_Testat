89855d9549a9d6650ae596808231247c183ef296
Gina <ginac.bauer@t-online.de>
2020-05-27 22:58:21 +0200

    Aktie & Aktiengesellschaft mit SQL Befehlen bereichert die 2.-die src-Dateien und nicht die bin-Dateien

--- src/börsenprogramm/Aktie.java
+++ src/börsenprogramm/Aktie.java
@@ -1,24 +1,25 @@
 package börsenprogramm;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Aktie {
 
-	private int id;
+	private final int id;
 	private int aktWert;
 	private ArrayList<Integer> werteHistorie = new ArrayList<Integer>();
+	Statement stat;
 
-	public Aktie(int id, int initialWert) {
+	public Aktie(Statement stat, int id, int initialWert) {
 		this.id = id;
 		this.aktWert = initialWert;
+		this.stat = stat;
 	}
 
 	public Integer getId() {
 		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public int getWert() {
@@ -29,8 +30,11 @@ public class Aktie {
 	 * Bevor der Wert der Aktie geändert wird, wird der aktuelle Wert zuerst in der
 	 * Werte-Historie gespeichert.
 	 */
-	public void setWert(int wert) {
+	public void setWert(int wert, Date aktDatum) throws SQLException {
 		this.werteHistorie.add(this.getWert());
+		stat.execute("INSERT INTO Wertehistorie (Aktie_ID, Wert, Datum) VALUES (" + this.id + ", " + this.aktWert + ", "
+				+ aktDatum+");");
+
 		this.aktWert = wert;
 	}
 }
