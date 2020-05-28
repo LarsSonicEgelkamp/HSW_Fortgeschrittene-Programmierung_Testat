39704ee521ec79d1fab0969df8c0a3fe6b447f0f
Lars Egelkamp <egelkamp00@gmx.de>
2020-05-28 22:48:16 +0200

    noch nicht ganz fertig - paar fehler sind noch drin

--- src/Filemanager/CSV_Manager.java
+++ src/Filemanager/CSV_Manager.java
@@ -1,9 +1,15 @@
 package Filemanager;
 
+import java.awt.List;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import börsenprogramm.Order;
 
@@ -58,8 +64,24 @@ public class CSV_Manager {
 
 			}
 		}
-		
-		
 
+	}
+
+	public ArrayList<String> readCSV(String filename)  {
+		ArrayList<String> records = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(",");
+				records.addAll(Arrays.asList(values));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return records;
 	}
 }
--- src/börsenprogramm/Boerse.java
+++ src/börsenprogramm/Boerse.java
@@ -1,6 +1,13 @@
 package börsenprogramm;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+import Filemanager.CSV_Manager;
 
 public class Boerse {
 
@@ -16,7 +23,7 @@ public class Boerse {
 	}
 
 	public void neueAktie(int id, int wert) {
-		Aktie aktie = new Aktie(id, wert);
+		Aktie aktie = new Aktie(null, id, wert);
 		this.aktienListe.add(aktie);
 	}
 
@@ -24,4 +31,28 @@ public class Boerse {
 		Depot depot = new Depot(id, inhaberName);
 		this.depotListe.add(depot);
 	}
+	
+	public ArrayList<String> alleAktienLesen (Statement stat, Connection con) {
+		ArrayList<String> alleAktien = new ArrayList<String>();
+		
+		try {
+			alleAktien = stat.("SELECT * FROM Aktie()");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return alleAktien;
+	}
+	
+	public void abfragenAktienHistorie (Statement stat, Connection con, Aktie aktienID) throws SQLException {
+		
+		ResultSet aktienHistorie = stat.executeQuery("SELECT ID FROM Wertehistorie WHERE ID = " + aktienID.getId()+";");
+		
+		if (aktienHistorie.next()) {
+			aktienListe.add(aktienID);
+		}
+	}
+	
+
 }
