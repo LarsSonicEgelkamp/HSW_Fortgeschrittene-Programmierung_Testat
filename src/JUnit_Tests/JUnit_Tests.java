7d31e0cf7db97a3cb89ccd89209242bea75fc155
Lars Egelkamp <egelkamp00@gmx.de>
2020-05-31 13:23:35 +0200

    JunitKlasse

--- src/JUnit_Tests/JUnit_Tests.java
+++ src/JUnit_Tests/JUnit_Tests.java
@@ -1,5 +1,30 @@
 package JUnit_Tests;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+//import de.loehrke.testen.junit.uebung02.Rechner;
+//	public class RechnerAdditionTest {
+//
+//		private Rechner rechner;
+//
+//		@Before
+//		public void initRechner() {
+//			this.rechner = new Rechner();
+//		}
+//
+//		@Test
+//		public void testAddition() {
+//			int actual = rechner.addiere(1, 2);
+//			Assert.assertTrue("Fehler bei der Addition", actual == 3);
+//		}
+//
+//	}
+
+
 public class JUnit_Tests {
-		
+	
+	public void init
+	
 }
